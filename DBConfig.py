00cc3458157518498b698d034c5805150a7947b8
IsuruUdara <108917676+Isuru-Rane@users.noreply.github.com>
2023-03-07 10:22:05 +0530

    Updated data entering logic

--- DBConfig.py
+++ DBConfig.py
@@ -6,11 +6,10 @@ myDB = myClient["skin_disease"]
 myTbl = myDB["disease"]
 
 disease_excel = pd.read_excel('SkinX-Disease.xlsx')
-myTbl.insert_many(disease_excel.to_dict(orient='record'))
+disease_list = disease_excel.to_dict(orient='record')
 
-myQ = {"Skin Disease": "Acne"}
-
-result = myTbl.find(myQ)
-
-for x in result:
-    print(x['Home Remedies/Treatments'])
+for x in disease_list:
+    query = {"Skin Disease": x["Skin Disease"]}
+    result = myTbl.find_one(query)
+    if result is None:
+        myTbl.insert_one(x)
